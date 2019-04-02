cc25ccd74a14d02811d81d4a7404f184d9c6ccad
satya0306 <satyapriya0306@gmail.com>
2019-04-02 13:13:31 +0530

    github-commit-listing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,110 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Time from './time';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import PropTypes from 'prop-types';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const FileList = ({files}) => {
+        return(
+            <table className="file-list" >
+                <tbody>
+                    {files.map(file => (
+                        <FileListItem key={file.id} file={file} />
+                    ))}
+                </tbody>
+            </table>
+        );
+}
+
+FileList.propTypes = {
+    files : PropTypes.array
+};
+
+const FileListItem = ({file}) => {
+    return(
+        <tr className="file-list-item" >
+            <FileName file={file} />
+            <CommitMessage commit = {file.latestCommit}/>
+            <td className="age" >
+                <Time time={file.updated_at} />
+            </td>
+        </tr>
+    );
+}
+
+FileListItem.propTypes = {
+    file : PropTypes.object.isRequired
+};
+
+const  FileIcon = ({file}) =>{
+    let icon = 'fa-file-text-o';
+    if(file.type ==='folder'){
+        icon = 'fa-folder';
+    }
+    return (
+        <td className="file-icon" >
+            <i className={`fa ${icon}`} />
+        </td>
+    );
+}
+
+FileIcon.propTypes = {
+    file : PropTypes.object.isRequired
+};
+
+
+function FileName({ file }) {
+    return (
+        <React.Fragment>
+            <FileIcon file={file} />
+            <td className="file-name" > {file.name} </td>
+        </React.Fragment>
+    );
+}
+FileName.propTypes = {
+    file : PropTypes.object.isRequired
+};
+
+const CommitMessage = ({commit}) =>{
+    return(
+        <td className="commit-message">
+            {commit.message}
+        </td>
+    );
+}
+CommitMessage.propTypes = {
+    commit : PropTypes.object.isRequired
+};
+
+const testFiles = [
+    {
+        id : 1,
+        name : 'src',
+        type : 'folder',
+        updated_at : "2016-07-11 21:24:00",
+        latestCommit : {
+            message : 'Initial commit'
+        }
+    },
+    {
+        id : 2,
+        name : 'tests',
+        type : 'folder',
+        updated_at : "2016-07-11 21:24:00",
+        latestCommit : {
+            message : 'Initial commit'
+        }
+    },
+    {
+        id : 3,
+        name : 'README',
+        type : 'file',
+        updated_at : "2016-07-18 21:24:00",
+        latestCommit : {
+            message : 'Added a readme'
+        }
+    },
+];
+
+ReactDOM.render(<FileList files={testFiles} />, document.getElementById('root'));
